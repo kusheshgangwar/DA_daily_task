2cfa3cd6280f38ec30bf793ef25fe9e46323ad27
KUSHESH GANGWAR (DeveloperWala) <168097522+kusheshgangwar@users.noreply.github.com>
2026-09-21 10:45:39 +0530

    Add files via upload

---
+++ Day 1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE company;
+use company;
+
+create table employees(
+E_id INT primary key,
+E_Roll_no varchar (20),
+Email varchar (100),
+phone_no varchar(30),
+salary varchar (50),
+Bonus varchar (50)
+);
+
+INSERT INTO ()
+select * from  employees;
+
+show databases;
+drop database comapany;
